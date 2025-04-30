3dae59948ba6227fe09ef5d9e27cb352d137959f
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-04-30 17:34:15 -0300

    props de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
     console.log('Oi');
 
     return (
         <>
-            <h1>Olá Mundo! (do app)</h1>
+            <Heading attr={123}>Olá mundo 1</Heading>
+            <Heading>Olá mundo 2</Heading>
+            <Heading>Olá mundo 3</Heading>
+            <Heading>Olá mundo 4</Heading>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos pariatur totam nihil, labore, aliquam iusto eius sapiente blanditiis alias, dicta impedit fugiat dolor doloribus aperiam quis error obcaecati velit eos?</p>
         </>
     );
