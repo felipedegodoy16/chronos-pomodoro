49ce37744dd6bfa3d9149dc6d54a9be7eaa942cd
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-04-30 16:32:11 -0300

    criação do componente App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export function App() {
+    console.log('Oi');
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Olá Mundo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Olá Mundo! (do app)</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos pariatur totam nihil, labore, aliquam iusto eius sapiente blanditiis alias, dicta impedit fugiat dolor doloribus aperiam quis error obcaecati velit eos?</p>
+        </>
+    );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render( // procura por um elemento que tem o id 'root' e renderiza o app dentro desse elemento
+import { App } from './App';
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
