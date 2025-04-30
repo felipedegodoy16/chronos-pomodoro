782c097aa8939f15b0d63a66862fe371c2dd3a6f
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-04-30 12:35:46 -0300

    fluxo de entrada do vite

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render( // procura por um elemento que tem o id 'root' e renderiza o app dentro desse elemento
   <StrictMode>
     <App />
   </StrictMode>,
