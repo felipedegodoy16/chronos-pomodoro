a7aaaf61f9495ea09c8809a35df870bf50aafb20
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-05-02 23:19:28 -0300

    criando containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 
 export function App() {
-    console.log('Oi');
-
     return (
         <>
-            <Heading attr={123}>Olá mundo 1</Heading>
-            <Heading>Olá mundo 2</Heading>
-            <Heading>Olá mundo 3</Heading>
-            <Heading>Olá mundo 4</Heading>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos pariatur totam nihil, labore, aliquam iusto eius sapiente blanditiis alias, dicta impedit fugiat dolor doloribus aperiam quis error obcaecati velit eos?</p>
+            <Container>
+                <Heading>LOGO</Heading>
+            </Container>
+            
+            <Container>
+                <Heading>MENU</Heading>
+            </Container>
         </>
     );
 }
