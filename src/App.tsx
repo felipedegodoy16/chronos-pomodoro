e6f45549f029009b25a1f0dce1e15d5563e0a19d
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-05-04 15:40:03 -0300

    cirando componente de button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
-
-import './styles/theme.css';
-import './styles/global.css';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+
+import './styles/theme.css';
+import './styles/global.css';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function App() {
     return (
@@ -38,7 +40,7 @@ export function App() {
                     </div>
                     
                     <div className="formRow">
-                        <button>Enviar</button>
+                        <DefaultButton icon={<PlayCircleIcon />} />
                     </div>
                 </form>
             </Container>
