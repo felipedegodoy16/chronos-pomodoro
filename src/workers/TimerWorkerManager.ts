de99b12cbc12a4ef819505e190bcceb7086f78c7
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-05-07 12:10:08 -0300

    criando o ewb worker

---
+++ src/workers/TimerWorkerManager.ts
@@ -0,0 +1,38 @@
+// a ideia de criar essa classe é para que ela gerencie nosso worker, ou seja, tudo que for ser usado pelo worker está aqui e fará tudo, assim, qualquer lugar do meu script que chamar o meu worker estará recebendo o mesmo worker, isso será necessário para que não fique se criando vários workers ao mesmo tempo
+
+let instance: TimerWorkerManager | null = null;
+
+export class TimerWorkerManager {
+    private worker: Worker;
+
+    private constructor() {
+        // a ideia aqui é privar para que não haja a instanciação dessa classe a não ser dentro dela mesma
+        this.worker = new Worker(new URL('./timerWorker.js', import.meta.url));
+    }
+
+    // método para retornar a instância da classe e caso ela ainda não tenha sido instanciada ela é instanciada aqui
+    static getInstance() {
+        // aqui é criado um método estático para ser chamado pela própria classe sem a necessidade de instanciação de um novo objeto da classe
+        if (!instance) {
+            instance = new TimerWorkerManager();
+        }
+
+        return instance;
+    }
+
+    // métodos do nosso worker, a ideia é usar as funções que o worker já tem porém como temos a classe e precisamos acessar o atributo worker dentro dela, criamos os métodos dentro da classe para isso
+
+    // método para postar/enviar a mensagem
+    postMessage(message: any) {
+        this.worker.postMessage(message);
+    }
+
+    onmessage(cb: (e: MessageEvent) => void) {
+        this.worker.onmessage = cb;
+    }
+
+    terminate() {
+        this.worker.terminate();
+        instance = null;
+    }
+}
---
+++ src/workers/timerWorker.js
@@ -0,0 +1,25 @@
+let isRunning = false; // vai ser um monitorador, já fazemos isso dentro do nosso 'TimerWorkerManager', porém, mesmo assim é bom garantir, então essa variável será uma flag que recebe false, assim quando entrar no nosso worker e estiver falso ele passa, se não ele retorna, nosso primeiro if faz isso
+
+self.onmessage = function (event) {
+    if (isRunning) return;
+
+    isRunning = true;
+
+    const state = event.data;
+    const { activeTask, secondsRemaining } = state;
+
+    const endDate = activeTask.startDate + secondsRemaining * 1000;
+    const now = Date.now();
+    let countDownSeconds = Math.ceil((endDate - now) / 100);
+
+    function tick() {
+        self.postMessage(countDownSeconds);
+
+        const now = Date.now();
+        countDownSeconds = Math.floor((endDate - now) / 100);
+
+        setTimeout(tick, 1000);
+    }
+
+    tick();
+};
