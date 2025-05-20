5b77625a67bcd00f3047dc6344e9c857e49a8119
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-05-20 11:38:12 -0300

    validando campos da settings

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,8 +4,51 @@ import { DefaultButton } from '../../components/DefaultButton';
 import { DefaultInput } from '../../components/DefaultInput';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
+import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { showMessage } from '../../adapters/showMessage';
 
 export function Settings() {
+    const { state } = useTaskContext();
+
+    const workTimeInput = useRef<HTMLInputElement>(null);
+    const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+    const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+    function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        showMessage.dismiss();
+
+        const formErrors = [];
+
+        const workTime = Number(workTimeInput.current?.value);
+        const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+        const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+        if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+            formErrors.push('Digite apenas números para todos os campos!');
+        }
+
+        if (workTime < 1 || workTime > 99) {
+            formErrors.push('Digite valores entre 1 e 99 para foco!');
+        }
+
+        if (shortBreakTime < 1 || shortBreakTime > 30) {
+            formErrors.push('Digite valores entre 1 e 30 para descanso curto!');
+        }
+
+        if (longBreakTime < 1 || longBreakTime > 60) {
+            formErrors.push('Digite valores entre 1 e 60 para descanso longo!');
+        }
+
+        if (formErrors.length > 0) {
+            formErrors.forEach((error) => {
+                showMessage.error(error);
+            });
+            return;
+        }
+    }
+
     return (
         <MainTemplate>
             <Container>
@@ -20,15 +63,24 @@ export function Settings() {
             </Container>
 
             <Container>
-                <form action="" className="form">
+                <form onSubmit={handleSaveSettings} action="" className="form">
                     <div className="formRow">
-                        <DefaultInput id="workTime" labelText="Foco" />
+                        <DefaultInput
+                            id="workTime"
+                            labelText="Foco"
+                            ref={workTimeInput}
+                            defaultValue={state.config.workTime}
+                            type="number"
+                        />
                     </div>
 
                     <div className="formRow">
                         <DefaultInput
                             id="shortBreakTime"
                             labelText="Descanso Curto"
+                            ref={shortBreakTimeInput}
+                            defaultValue={state.config.shortBreakTime}
+                            type="number"
                         />
                     </div>
 
@@ -36,6 +88,9 @@ export function Settings() {
                         <DefaultInput
                             id="longBreakTime"
                             labelText="Descanso Longo"
+                            ref={longBreakTimeInput}
+                            defaultValue={state.config.longBreakTime}
+                            type="number"
                         />
                     </div>
 
